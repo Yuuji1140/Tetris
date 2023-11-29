05e49e8b6f70d15f0c381197729215ec736bf899
Yuuji1140 <vietnguyen2358@gmail.com>
2023-11-29 01:33:07 -0800

    Initial commit

---
+++ src/Main/Manager.java
@@ -0,0 +1,32 @@
+package Main;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+public class Manager {
+    final int WIDTH = 360;
+    final int HEIGHT = 600;
+    public static int xLeft;
+    public static int xRight;
+    public static int yTop;
+    public static int yBottom;
+    
+    // Set gameplay frame
+    public Manager() {
+        xLeft = (GamePanel.WIDTH/2) - (WIDTH/2);
+        xRight = xLeft + WIDTH;
+        yTop = 50;
+        yBottom = yTop + HEIGHT;
+    }
+
+    public void update() {
+
+    }
+
+    public void draw(Graphics2D g2) {
+        g2.setColor(Color.WHITE);
+        g2.setStroke(new BasicStroke(4f));
+        g2.drawRect(xLeft - 4, yTop - 4, WIDTH + 8, HEIGHT + 8);
+    }
+}
