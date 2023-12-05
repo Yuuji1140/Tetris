e021e51ecd92510d8e4cd9115430685be69ff33a
Yuuji1140 <vietnguyen2358@gmail.com>
2023-12-04 23:44:14 -0800

    Update Manager

--- src/Main/Manager.java
+++ src/Main/Manager.java
@@ -2,7 +2,9 @@ package Main;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 
 public class Manager {
     final int WIDTH = 360;
@@ -29,5 +31,13 @@ public class Manager {
         g2.setColor(Color.WHITE);
         g2.setStroke(new BasicStroke(4f));
         g2.drawRect(xLeft - 4, yTop - 4, WIDTH + 8, HEIGHT + 8);
+
+        // Draw Next Block Frame
+        int x = xRight + 50;
+        int y = yBottom - 200;
+        g2.drawRect(x, y, 150, 150);
+        g2.setFont(new Font("Arial", Font.BOLD, 30));
+        g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        g2.drawString("NEXT", x + 35, y + 35);
     }
 }
